e152d2364ac8df6c70d65ba9560c6f72a69bac89
veratrofimova <kuzik-86@mail.ru>
2024-10-28 23:49:44 +0300

    Add project files.

---
+++ DZ_Lesson_21_LoopsAndRecursions/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿namespace DZ_Lesson_21_LoopsAndRecursions
+{
+    public class Fibonacci
+    {
+        public static void Recursive(long f1, long f2, int i, long n)
+        { 
+            if (i + 2 < n)
+            {
+                long result = f1 + f2;
+
+                Console.WriteLine($"F({i + 3}) = F({i + 2}) + F({i + 1}) = {f1} + {f2} = {result}");
+             
+                Recursive(f2, result, i + 1, n);
+            }
+        }
+
+        public static void Loop(long f1, long f2, int i, long n)
+        {
+            do
+            {
+                long result = f1 + f2;
+                Console.WriteLine($"F({i + 3}) = F({i + 2}) + F({i + 1}) = {f1} + {f2} = {result}");
+                f2 = f1;
+                f1 = result;
+                i++;
+            } while (i + 2 < n);
+        }
+    }
+}
---
+++ DZ_Lesson_21_LoopsAndRecursions/Program.cs
@@ -0,0 +1,33 @@
+﻿using DZ_Lesson_21_LoopsAndRecursions;
+using System.Diagnostics;
+
+Console.WriteLine("Добро пожаловать в программу: Циклы и рекурсии");
+Console.WriteLine("Реализованы методы нахождения n-го члена последовательности Фибоначчи по формуле F(n) = F(n-1) + F(n-2)");
+Console.WriteLine("Введите значение n");
+
+try
+{
+    long n = long.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    Stopwatch stopwatch = Stopwatch.StartNew();
+
+    stopwatch.Start();
+    Fibonacci.Recursive(1, 1, 0, n);
+    stopwatch.Stop();
+    Console.WriteLine($"Нахождение через рекурсию заняло: {stopwatch.ElapsedMilliseconds} миллисекунд\r\n");
+    stopwatch.Reset();
+
+    stopwatch.Start();
+    Fibonacci.Loop(1, 1, 0, n);
+    stopwatch.Stop();
+    Console.WriteLine($"Нахождение через цикл заняло: {stopwatch.ElapsedMilliseconds} миллисекунд");    
+}
+catch (FormatException)
+{
+    Console.WriteLine("Введите число");
+}
+catch (Exception)
+{
+    throw;
+}
